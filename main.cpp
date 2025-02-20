98600f3ad3648393ee33e8c481e2659964a597b0
nicra <nicra@itu.dk>
2025-02-20 11:24:20 +0100

    handling 0 args and 2 args

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 #include <tuple>
 #include <fstream>
 #include <vector>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <pthread.h>
+
 
 using namespace std;
 
 const string fileName = "random_integers.txt";
-int hash_bits = 4;
+int HASH_BITS = 4;
+int NUM_THREADS = 4;
 
 // Cast one string line to a integer  tupleto  
 tuple<uint64_t, uint64_t> cast_to_tuple(std::string line) {
@@ -21,6 +27,7 @@ tuple<uint64_t, uint64_t> cast_to_tuple(std::string line) {
     return result;
 }
 
+// Read generated file with random tuples of usigned 64 bit integers
 vector<tuple<uint64_t, uint64_t>> read_file() {
     ifstream file(fileName);
     string str;
@@ -36,34 +43,62 @@ vector<tuple<uint64_t, uint64_t>> read_file() {
 
 int hash_function(uint64_t key) {
     // Implement a hash function
-    return key % (1 << hash_bits);
+    return key % (1 << HASH_BITS);
 }
 
-int main(int argc, char *argv[]) {
-
-    // Read generated file with random tuples of usigned 64 bit integers
-    if(argc < 1) {
-        cout << "Usage: " << argv[0] << " <number of hash bits>" << endl;
-        return 1;
-    }
-    auto hash_bit_arg = stoi(argv[1]);
-    if(hash_bit_arg <= 0 || hash_bit_arg > 18) {
-        cout << "Please enter a positive number between 1-18" << endl;
-        return 1;
-    }
-
-    hash_bits = hash_bit_arg;
-
-    auto data = read_file();    
-    // for(auto tuple : data){
-    //     cout << get<0>(tuple) << " " << get<1>(tuple) << endl;
-    // }
+void print_hash_values(vector<tuple<uint64_t, uint64_t>> data) {
     for(int i = 0; i < data.size(); i++){
         cout << hash_function(get<0>(data[i])) << " " << get<1>(data[i]) << endl;
     }
-    // auto first_key = get<0>(data[1]);
-    // auto hashed_key = hash_function(first_key);
-    // cout << "First key: " << first_key << " Hashed key: " << hashed_key << endl;
+}
+void work(int thread_index) {
+    std::this_thread::sleep_for(chrono::milliseconds(thread_index));
+    cout << "Thread #" << thread_index << ": on CPU " << sched_getcpu() << endl;
+}
+
+void thread_work(vector<tuple<uint64_t, uint64_t>> data) {
+    std::vector<std::thread> threads(NUM_THREADS);
+    for (int i = 0; i < NUM_THREADS; ++i) {
+      threads[i] = thread(work, i);
+    }
+  
+    for (auto& t : threads) {
+      t.join();
+    }
+  }
+
+
+int main(int argc, char *argv[]) {
+    int hash_bit_arg;
+    int thread_count_arg;
+    if(argc <= 2 ) {
+        cout << "Using default values hash_bits=" << HASH_BITS << " thread_count=" << NUM_THREADS << endl;
+    } else {
+        hash_bit_arg = stoi(argv[1]);
+        thread_count_arg = stoi(argv[2]);
+        
+        if(hash_bit_arg <= 0 || hash_bit_arg > 18) {
+            cout << "Please enter a positive number between 1-18" << endl;
+            return 1;
+        }
+    
+        if(thread_count_arg <= 0 || thread_count_arg > 32) {
+            cout << "Please enter a positive number between 1-32" << endl;
+            return 1;
+        }
+        
+        HASH_BITS = hash_bit_arg;
+        NUM_THREADS = thread_count_arg;
+    }
+    
+    
+    // Read data from file
+    auto data = read_file();    
+
+    // print_hash_values(data);
+
+    // Do the work in threads
+    thread_work(data);
 
     return 0;
 }
