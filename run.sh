47910f382c465935976f6054e91f49e74a0f89ec
nicra <nicra@itu.dk>
2025-02-22 23:42:02 +0100

    first results from bash

--- run.sh
+++ run.sh
@@ -8,16 +8,30 @@ OUTPUT="out.o"
 # g++ -o $OUTPUT $SOURCE
 RESULTS_FILE="results.txt"
 
-# Check if compilation was successful
+# Delete the results file if it exists
 if [ -f $RESULTS_FILE ]; then
    rm $RESULTS_FILE
    echo "Removed old results file"
 fi
 
+# Create the results file
+touch $RESULTS_FILE
+
+# Create First line of results file
+ALL_THREADS=""
+for i in {0..5} # threads
+do    
+    ALL_THREADS+="$i "
+done
+echo "$ALL_THREADS" >> $RESULTS_FILE
+
 for i in {2..4} # hashbits
 do
+    # -n flag to not print newline
+    echo -n "$i," >> $RESULTS_FILE
     for j in {0..5} # number of threads 
     do
         ./$OUTPUT $i 2^$j 1
     done
+    echo "" >> $RESULTS_FILE
 done
