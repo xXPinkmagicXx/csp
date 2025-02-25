122fdea2454a504739fc8174e3507d93b9c40788
gabortodor <gabortodor@gmail.com>
2025-02-25 14:08:59 +0100

    Add headers and cpps for methods

--- abstract_method.h
+++ abstract_method.h
@@ -5,48 +5,29 @@
 #include <tuple>
 #include <vector>
 #include <thread>
+#include <cstdint>
 
 using namespace std;
 
 class AbstractMethod {
-    public:
-        int HASH_BITS;
-        int NUM_THREADS;
-        int DATA_SIZE = 0;
-        int VERBOSE = 0;
+public:
+    int HASH_BITS;
+    int NUM_THREADS;
+    int DATA_SIZE;
+    int VERBOSE;
+    
+    AbstractMethod(int hash_bits, int num_threads, int data_size, int verbose);
+    virtual ~AbstractMethod() = default;
 
-        AbstractMethod(int hash_bits, int num_threads, int data_size, int verbose) {
-            HASH_BITS = hash_bits;
-            NUM_THREADS = num_threads;
-            DATA_SIZE = data_size;
-            VERBOSE = verbose;
-          }
+    int get_num_partitions() const;
+    int hash_function(uint64_t key) const;
+    void print_hash_values(const vector<tuple<uint64_t, uint64_t>>& data) const;
 
-        int get_num_partitions() {
-            return 1 << HASH_BITS;
-        }
+    virtual void work(int thread_index, const vector<tuple<uint64_t, uint64_t>>& data, int start_index, int bucket_size) = 0;
+    virtual void thread_work(const vector<tuple<uint64_t, uint64_t>>& data) = 0;
+    virtual void print_buffers_everything() = 0;
+    virtual void print_buffers_partition_entries() = 0;
+    virtual void print_buffers_partition_statistics() = 0;
+};
 
-        int hash_function(uint64_t key) {
-            return key % (1 << HASH_BITS);
-        }
-
-        void print_hash_values(vector<tuple<uint64_t, uint64_t>> data) {
-            for(int i = 0; i < data.size(); i++) {
-                cout << hash_function(get<0>(data[i])) << " " << get<1>(data[i]) << endl;
-            }
-        }
-
-        virtual ~AbstractMethod() = default;
-
-        virtual void work(int thread_index, const vector<tuple<uint64_t, uint64_t>>& data, int start_index, int bucket_size) = 0;
-       
-        virtual void thread_work(const vector<tuple<uint64_t, uint64_t>>& data) = 0;
-
-        virtual void print_buffers_everything() = 0;
-
-        virtual void print_buffers_partition_entries() = 0;
-
-        virtual void print_buffers_partition_statistics() = 0;
- };
-
- #endif
+#endif
--- independent_method.h
+++ independent_method.h
@@ -13,88 +13,16 @@
 using namespace std;
 
 class IndependentMethod : public AbstractMethod {
-    public:
-        vector<vector<vector<tuple<uint64_t, uint64_t>>>> buffer_collection;
+public:
+    vector<vector<vector<tuple<uint64_t, uint64_t>>>> buffer_collection;
 
-        using AbstractMethod::AbstractMethod;
+    using AbstractMethod::AbstractMethod;
 
-        void work(int thread_index, const vector<tuple<uint64_t, uint64_t>>& data, int start_index, int bucket_size) {
-            vector<vector<tuple<uint64_t, uint64_t>>> buffer(get_num_partitions());
-            if(VERBOSE == 2) {
-                cout << "Thread #" << thread_index << ": start_index= " << start_index << endl;
-            }
-            // Identify the partition by hash function
-            for(int i = start_index; i < start_index + bucket_size; i++) {
-                // Hash key to get the partition key
-                auto key = get<0>(data[i]);
-                auto partition_key = hash_function(key);
-                
-                // Add tuple to local buffer
-                buffer[partition_key].push_back(data[i]);
-            }
-            buffer_collection[thread_index] = move(buffer);
-            if(VERBOSE == 2) {
-                cout << "Thread #" << thread_index << " completed... "<< endl;
-            }
-        }      
+    void work(int thread_index, const vector<tuple<uint64_t, uint64_t>>& data, int start_index, int bucket_size);
+    void thread_work(const vector<tuple<uint64_t, uint64_t>>& data);
+    void print_buffers_everything();
+    void print_buffers_partition_entries();
+    void print_buffers_partition_statistics();
+};
 
-        void thread_work(const vector<tuple<uint64_t, uint64_t>>& data) {
-            buffer_collection.resize(NUM_THREADS);
-            auto bucket_size = data.size() / NUM_THREADS;
-            if(VERBOSE == 2) {
-                cout << "Starting with " << NUM_THREADS << " threads and bucket size " << bucket_size << endl;
-            }
-            
-            // Initialize threads
-            vector<thread> threads(NUM_THREADS);
-            for (int i = 0; i < NUM_THREADS; ++i) {
-              threads[i] = thread(&IndependentMethod::work, this, i, data, i * bucket_size, bucket_size);
-
-            }
-            // Join threads
-            for (auto& t : threads) {
-              t.join();
-            }
-        }
-
-        void print_buffers_everything() {
-            for (int i = 0; i < buffer_collection.size(); i ++) {
-                for(int j = 0; j < buffer_collection[i].size(); j++) {
-                    for(int k = 0; k < buffer_collection[i][j].size(); k++) {
-                        cout << "Thread: " << i << "Partition: " << j
-                        << " Key: " << get<0>(buffer_collection[i][j][k])
-                        << " Value: " << get<1>(buffer_collection[i][j][k]) << endl;
-                    }
-                }
-            }
-        }
-        
-        void print_buffers_partition_entries() {
-            for(int i = 0; i < buffer_collection.size(); i++) {
-                for(int j = 0; j < buffer_collection[i].size(); j++) {
-                    auto partition_size = buffer_collection[i][j].size();
-                    cout << "Thread: " << i << "Partition: " << j << "# entries: " << partition_size << endl;
-                }
-            }
-        }
-
-        void print_buffers_partition_statistics() {
-            int num_partitions = get_num_partitions();
-            auto counter_arr = new int[num_partitions];
-            
-            float mean = (DATA_SIZE / num_partitions) / NUM_THREADS;
-            float std_dev = 0.0;
-        
-            for (int i = 0; i < buffer_collection.size(); i++) {
-                for(int j = 0; j < buffer_collection[i].size(); j++) {
-                    auto partition_size = buffer_collection[i][j].size();
-                    auto variance = partition_size - mean;
-                    std_dev += variance * variance; 
-                }
-            }
-            std_dev = sqrt(std_dev / num_partitions);
-            cout << "Expected Partion Size: " << mean << " ±" << std_dev << endl;
-        }
- };
-
- #endif
+#endif
